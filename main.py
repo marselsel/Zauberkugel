ca5632dc7aca7d4597862feae99fd5b2c6e9e322
Marcel-Felix Krause <marfelkra@gmail.com>
2021-03-29 00:05:24 +0200

    plt done

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+df = pd.read_csv("AAPL.csv")
 
+# Sort DataFrame by date
+df = df.sort_values('Date')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
+plt.xticks(range(0,df.shape[0],500),df['Date'].loc[::500],rotation=45)
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Mid Price',fontsize=18)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.show()
